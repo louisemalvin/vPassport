391cfe6cb18eedb5e886068cefa6d667b9dc1791
Louise <louise.malvin12@gmail.com>
2023-07-15 15:22:27 +0200

    Add ecnryption to user passport

--- app/src/main/java/com/example/vpassport/util/serializers/PassportSerializer.kt
+++ app/src/main/java/com/example/vpassport/util/serializers/PassportSerializer.kt
@@ -4,19 +4,29 @@ import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
 import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
 import com.example.vpassport.Passport
+import com.example.vpassport.util.CryptoManager
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
 object PassportSerializer : Serializer<Passport> {
+    private val cryptoManager = CryptoManager()
     override val defaultValue: Passport = Passport.getDefaultInstance()
 
     override suspend fun readFrom(input: InputStream): Passport {
         try {
-            return Passport.parseFrom(input)
+            val decryptedBytes = cryptoManager.decrypt(input)
+            val decryptedStream = ByteArrayInputStream(decryptedBytes)
+            return Passport.parseFrom(decryptedStream)
         } catch (exception: InvalidProtocolBufferException) {
             throw CorruptionException("Cannot read proto.", exception)
         }
     }
 
-    override suspend fun writeTo(t: Passport, output: OutputStream) = t.writeTo(output)
+    override suspend fun writeTo(t: Passport, output: OutputStream) {
+        val byteStream = ByteArrayOutputStream()
+        t.writeTo(byteStream)
+        cryptoManager.encrypt(byteStream.toByteArray(), output)
+    }
 }
